84926be4e36776e3c078dc3f217864833f45609e
Gia Bao <21521866@gm.uit.edu.vn>
2023-11-20 11:38:35 +0700

    restructured folder

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const rooms = [
+  {
+    id: "1",
+    name: "Grand Deluxe Room",
+    image: "../assets/images/room-1.png",
+    price: 2500,
+    guest: 3,
+    size: 47,
+  },
+  {
+    id: "2",
+    name: "Grand Deluxe Room",
+    image: "../assets/images/room-1.png",
+    price: 2500,
+    guest: 3,
+    size: 47,
+  },
+  {
+    id: "3",
+    name: "Grand Deluxe Room",
+    image: "../assets/images/room-1.png",
+    price: 2500,
+    guest: 3,
+    size: 47,
+  },
+  {
+    id: "4",
+    name: "Grand Deluxe Room",
+    image: "../assets/images/room-1.png",
+    price: 2500,
+    guest: 3,
+    size: 47,
+  },
+];
+
+export default rooms;
